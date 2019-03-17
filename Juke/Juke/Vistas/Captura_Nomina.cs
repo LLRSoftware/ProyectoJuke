891cb714cf4814f11c4402b84f3a39b5c5908a56
Leonardo Loza <leonardolozar@hotmail.com>
2019-03-17 14:48:43 -0600

    Captura de nomina

---
+++ Juke/Juke/Vistas/Captura_Nomina.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juke.Vistas
+{
+    public partial class Captura_Nomina : Form
+    {
+        public Captura_Nomina()
+        {
+            InitializeComponent();
+        }
+    }
+}

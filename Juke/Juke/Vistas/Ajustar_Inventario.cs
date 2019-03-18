ba997f02dac9e11ebb4a43175a4298654c85fd3a
Leonardo Loza <leonardolozar@hotmail.com>
2019-03-17 19:21:06 -0600

    Pantallas

---
+++ Juke/Juke/Vistas/Ajustar_Inventario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juke.Vistas
+{
+    public partial class Ajustar_Inventario : Form
+    {
+        public Ajustar_Inventario()
+        {
+            InitializeComponent();
+        }
+    }
+}

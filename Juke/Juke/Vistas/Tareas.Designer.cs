de89f94ec7e237648c6cf5b54b83f3fc80b00680
Liliana Romero <liliana22rom@gmail.com>
2019-04-11 01:10:13 -0500

    Ventana Tareas

--- Juke/Juke/Vistas/Tareas.Designer.cs
+++ Juke/Juke/Vistas/Tareas.Designer.cs
@@ -29,74 +29,114 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnModificar = new System.Windows.Forms.Button();
-            this.btnNuevaTarea = new System.Windows.Forms.Button();
-            this.btnSalir = new System.Windows.Forms.Button();
             this.dgvTareas = new System.Windows.Forms.DataGridView();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnNuevaTarea = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvTareas)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.SlateGray;
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.btnModificar);
             this.panel1.Controls.Add(this.btnNuevaTarea);
             this.panel1.Controls.Add(this.btnSalir);
             this.panel1.Controls.Add(this.dgvTareas);
-            this.panel1.Location = new System.Drawing.Point(-3, 1);
+            this.panel1.Location = new System.Drawing.Point(-3, -4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(805, 450);
+            this.panel1.Size = new System.Drawing.Size(789, 541);
             this.panel1.TabIndex = 0;
-            // 
-            // btnModificar
-            // 
-            this.btnModificar.Location = new System.Drawing.Point(681, 293);
-            this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(75, 23);
-            this.btnModificar.TabIndex = 3;
-            this.btnModificar.Text = "Modificar";
-            this.btnModificar.UseVisualStyleBackColor = true;
-            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
-            // 
-            // btnNuevaTarea
-            // 
-            this.btnNuevaTarea.Location = new System.Drawing.Point(681, 213);
-            this.btnNuevaTarea.Name = "btnNuevaTarea";
-            this.btnNuevaTarea.Size = new System.Drawing.Size(75, 23);
-            this.btnNuevaTarea.TabIndex = 2;
-            this.btnNuevaTarea.Text = "Nueva";
-            this.btnNuevaTarea.UseVisualStyleBackColor = true;
-            this.btnNuevaTarea.Click += new System.EventHandler(this.btnNuevaTarea_Click);
-            // 
-            // btnSalir
-            // 
-            this.btnSalir.Location = new System.Drawing.Point(681, 399);
-            this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(75, 23);
-            this.btnSalir.TabIndex = 1;
-            this.btnSalir.Text = "Salir";
-            this.btnSalir.UseVisualStyleBackColor = true;
-            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // dgvTareas
             // 
+            this.dgvTareas.BackgroundColor = System.Drawing.Color.White;
             this.dgvTareas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvTareas.Location = new System.Drawing.Point(15, 167);
+            this.dgvTareas.Location = new System.Drawing.Point(24, 75);
             this.dgvTareas.Name = "dgvTareas";
             this.dgvTareas.RowTemplate.Height = 24;
-            this.dgvTareas.Size = new System.Drawing.Size(600, 189);
+            this.dgvTareas.Size = new System.Drawing.Size(749, 275);
             this.dgvTareas.TabIndex = 0;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.btnSalir.ForeColor = System.Drawing.Color.White;
+            this.btnSalir.Location = new System.Drawing.Point(382, 477);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(110, 36);
+            this.btnSalir.TabIndex = 1;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnNuevaTarea
+            // 
+            this.btnNuevaTarea.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnNuevaTarea.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNuevaTarea.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.btnNuevaTarea.ForeColor = System.Drawing.Color.White;
+            this.btnNuevaTarea.Location = new System.Drawing.Point(663, 477);
+            this.btnNuevaTarea.Name = "btnNuevaTarea";
+            this.btnNuevaTarea.Size = new System.Drawing.Size(110, 36);
+            this.btnNuevaTarea.TabIndex = 2;
+            this.btnNuevaTarea.Text = "Nueva";
+            this.btnNuevaTarea.UseVisualStyleBackColor = false;
+            this.btnNuevaTarea.Click += new System.EventHandler(this.btnNuevaTarea_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.btnModificar.ForeColor = System.Drawing.Color.White;
+            this.btnModificar.Location = new System.Drawing.Point(529, 477);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(110, 36);
+            this.btnModificar.TabIndex = 3;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = false;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(802, 48);
+            this.panel2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.Window;
+            this.label1.Location = new System.Drawing.Point(8, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tareas";
             // 
             // Tareas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(782, 533);
             this.Controls.Add(this.panel1);
             this.Name = "Tareas";
             this.Text = "Tareas";
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvTareas)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -104,9 +144,11 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.DataGridView dgvTareas;
         private System.Windows.Forms.Button btnModificar;
         private System.Windows.Forms.Button btnNuevaTarea;
         private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.DataGridView dgvTareas;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
     }
 }
